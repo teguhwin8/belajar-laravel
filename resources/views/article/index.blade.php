cf29067af221bda16464c20db705be76e7688ba8
Teguh Widodo <teguhwin8@gmail.com>
2021-03-30 12:23:18 +0700

    Integrate Boostrap css

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Ini halaman artikel</h1>
+  <h1>Ini halaman artikel</h1>
 
-    @foreach ($articles as $article)
-        <p><strong>{{ $article['title'] }}</strong></p>
-        <p>{{ $article['subject'] }}</p>
-    @endforeach
+  @foreach ($articles as $article)
+    <div class="card mb-3">
+			<div class="card-body">
+				<p><strong>{{ $article['title'] }}</strong></p>
+				<p>{{ $article['subject'] }}</p>
+			</div>
+    </div>
+  @endforeach
 
-    {{ $articles->links() }}
+  {{ $articles->links() }}
 @endsection

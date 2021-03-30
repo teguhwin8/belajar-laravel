565375ef21ce19d1ae775bce17560aba771be65b
Teguh Widodo <teguhwin8@gmail.com>
2021-03-30 13:30:04 +0700

    Form validation

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,25 +6,52 @@ use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
-{
+{    
+    /**
+     * index
+     *
+     * @return void
+     */
     public function index()
     {
         $articles = Article::paginate(5);
         return view('article.index', compact('articles'));
     }
-
+    
+    /**
+     * show
+     *
+     * @param  mixed $slug
+     * @return void
+     */
     public function show($slug)
     {
         return view('article.single', compact('slug'));
     }
-
+    
+    /**
+     * create
+     *
+     * @return void
+     */
     public function create()
     {
         return view('article.create');
     }
-
+    
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|min:3|max:255',
+            'subject' => 'required|min:10'
+        ]);
+
         $article = new Article;
         $article->title = $request->title;
         $article->subject = $request->subject;
--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -6,11 +6,17 @@
 		@csrf
 		<div class="mb-3">
 			<label for="title" class="form-label">Judul Artikel</label>
-			<input type="text" class="form-control" id="title" name="title">
+			<input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+			@error('title')
+				<div class="invalid-feedback my-1">{{ $message }}</div>
+			@enderror
 		</div>
 		<div class="mb-3">
 			<label for="subject" class="form-label">Subject</label>
-			<textarea class="form-control" id="subject" rows="5" name="subject"></textarea>
+			<textarea class="form-control @error('subject') is-invalid @enderror" id="subject" rows="5" name="subject">{{ old('subject') }}</textarea>
+			@error('subject')
+				<div class="invalid-feedback my-1">{{ $message }}</div>
+			@enderror
 		</div>
 		<button type="submit" class="btn btn-primary">Submit</button>
 	</form>

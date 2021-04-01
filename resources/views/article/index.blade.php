6aedf0fc7f4f783cb82765c5995f2f5296d2493b
Teguh Widodo <teguhwin8@gmail.com>
2021-04-01 22:58:18 +0700

    Include

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,23 +3,27 @@
 @section('title', 'Artikel')
 
 @section('content')
-  <h1>Ini halaman artikel</h1>
-  <a href="/article/create" class="btn btn-primary mb-3">Bikin Artikel +</a>
-  @foreach ($articles->chunk(3) as $article_chunk)
-    <div class="row">
-      @foreach ($article_chunk as $article)
-        <div class="col-md-4">
-          <div class="rounded shadow-sm p-3 mb-3 position-relative">
+  <div class="min-vh-100">
+    <h1>Ini halaman artikel</h1>
+    <a href="/article/create" class="btn btn-primary mb-3">Bikin Artikel +</a>
+    @foreach ($articles->chunk(3) as $article_chunk)
+      <div class="row">
+        @foreach ($article_chunk as $article)
+          <div class="col-md-4">
+            <div class="rounded shadow-sm p-3 mb-3 position-relative">
               <p><strong>{{ $article->title }}</strong></p>
               <p>{{ $article->subject }}</p>
               <a href="/article/{{ $article->slug }}" class="stretched-link"></a>
+            </div>
           </div>
-        </div>
-      @endforeach
-    </div>
-  @endforeach
+        @endforeach
+      </div>
+    @endforeach
 
-  <div class="d-flex justify-content-center mt-3">
-    {{ $articles->links() }}
+    <div class="d-flex justify-content-center mt-3">
+      {{ $articles->links() }}
+    </div>
   </div>
+
+  @include('layouts.footer')
 @endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="p-3 bg-dark text-muted">
+    <small>Copyright © 2021 <br> Sekolah Koding</small>
+</footer>
